09c9aa538d33532300a76738d4da5830783b3a04
santiagoburgos <sburgos@itba.edu.ar>
2021-09-16 15:01:57 -0300

    out to logger

--- tpe1-g6/client/src/main/java/ar/edu/itba/pod/client/FlightEventCallbackImpl.java
+++ tpe1-g6/client/src/main/java/ar/edu/itba/pod/client/FlightEventCallbackImpl.java
@@ -1,23 +1,28 @@
 package ar.edu.itba.pod.client;
 
 import ar.edu.itba.pod.api.callbacks.FlightEventCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.rmi.RemoteException;
 
 public class FlightEventCallbackImpl implements FlightEventCallback {
+
+    private static Logger logger = LoggerFactory.getLogger(FlightEventCallbackImpl.class);
+
     @Override
     public void flightAssigned(int flightId, String destCode, String runway, int flightsAhead) throws RemoteException {
-        System.out.println("Flight + "+flightId+" with destiny "+destCode+" was assigned to runway "+runway+" and there are "+flightsAhead+" flights waiting ahead." );
+        logger.info("Flight + "+flightId+" with destiny "+destCode+" was assigned to runway "+runway+" and there are "+flightsAhead+" flights waiting ahead." );
     }
 
     @Override
     public void flightUpdated(int flightId, String destCode, String runway, int flightsAhead) throws RemoteException {
-        System.out.println("A flight departed from runway + "+runway+". Flight "+flightId+" waith destiny "+destCode+" has "+flightsAhead+" flights waiting ahead." );
+        logger.info("A flight departed from runway + "+runway+". Flight "+flightId+" waith destiny "+destCode+" has "+flightsAhead+" flights waiting ahead." );
     }
 
     @Override
     public void flightDeparted(int flightId, String destCode, String runway) throws RemoteException {
-        System.out.println("Flight " + flightId +  " with destiny "+destCode+" departed on runway "+runway+"." );
+        logger.info("Flight " + flightId +  " with destiny "+destCode+" departed on runway "+runway+"." );
 
     }
 }
